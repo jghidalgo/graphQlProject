8497e4ceb03cb2672fb5f3a6522ab5a9564f9b48
jghidalgo <joanrodr@gmail.com>
2022-04-08 23:22:47 -0400

    Installing new dependencies

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
-  res.send('Hello World')
-})
+    res.send('Hello World');
+});
 
-app.listen(3000)
+app.listen(3000);

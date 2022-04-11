5faed52231d7e603f5bebf93f371a7c552ab0759
jghidalgo <joanrodr@gmail.com>
2022-04-10 23:43:26 -0400

    Add mutations

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World');
-});
+app.use(
+    '/graphql',
+    graphqlHttp({
+        schema: buildSchema(`
+      type RootQuery {
+          events: [String!]! 
+      }
+      type RootMutation {
+          createEvent(name: String): String
+      }
+      schema {
+            query: RootQuery
+            mutation: RootMutation
+      }
+  `),
+        rootValue: {}
+    })
+);
 
 app.listen(3000);
